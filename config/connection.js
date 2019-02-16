27b5a4ff5ba3c8cb7ac5c05152684b3af66f55a0
variable56 <variable56@gmail.com>
2019-02-15 19:45:29 -0700

    semi working

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.password,
+    database: "burgers_db"
+  });
+
+  // Makes the connection to SQL database.
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+//Exports the connection for use in other files
+module.exports = connection
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pastrami Burger', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bleu Bacon', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guacamole Bacon', 1);
+
