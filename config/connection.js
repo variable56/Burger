2d06ab39f3b2e39aa70b5ce913e4de450e1f99f2
variable56 <variable56@gmail.com>
2019-02-19 21:15:34 -0700

    Heroku prep

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.password,
-    database: "burgers_db"
-  });
+var connection;
+
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: process.env.password,
+      database: "burgers_db"
+    });
+  }
 
   // Makes the connection to SQL database.
 connection.connect(function(err) {
